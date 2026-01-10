1e5997c6e3eaca4a608d4411c37d7fffb6eda5d8
sntr <sourcemaster@digi-craft.de>
2026-01-10 16:41:01 +0100

    same

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
 	plugins: [react()],
+	base: "/",
 	server: {
 		port: 3011,
 	},
 	preview: {
 		port: 3011,
+		allowedHosts: ["digi-craft.de"],
 	},
 });
