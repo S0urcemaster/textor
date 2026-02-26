c055826cb9aef11695ded98bcd6fa6a9365e2fbc
sntr <sourcemaster@digi-craft.de>
2026-02-26 15:09:16 +0100

    up

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -176,7 +176,7 @@ export default function () {
 	return (
 		<div className="appRoot" style={{
 			background: `linear-gradient(to right, ${system.settings.colors.materialLo}, ${system.settings.colors.materialHi})`,
-			width: `min(${system.settings.width}px, var(--app-max-width, 100vw))`,
+			width: `min(${system.settings.width}px, 100vw)`,
 			position: "relative",
 			overflow: "hidden"
 		}}>
